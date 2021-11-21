d49d91948e40c1330b70b1cd0dd18c7398c95fb9
Maple <maxlappleofficial@gmail.com>
2021-11-21 14:00:03 +0300

    Initial Commit

---
+++ MatrixCalc/MatrixCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixCalcApp.swift
+//  MatrixCalc
+//
+//  Created by Максим on 21.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MatrixCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
